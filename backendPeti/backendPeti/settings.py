2bb52598864029eb8ebdfaf658aaee7353326808
dimasinr <dimas.indra773@gmail.com>
2023-01-11 19:37:46 +0700

    auth

--- backendPeti/backendPeti/settings.py
+++ backendPeti/backendPeti/settings.py
@@ -143,7 +143,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-        # 'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'USER_DETAILS_SERIALIZER': 'userapp.serializer.UserDetailsSerializer'
 }
