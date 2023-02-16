1488c09d5decc743111a6133445ab4a4bd50987c
dimasinr <dimas.indra773@gmail.com>
2023-02-16 16:54:52 +0700

    DA

--- backendPeti/backendPeti/settings_prod.py
+++ backendPeti/backendPeti/settings_prod.py
@@ -1,7 +1,7 @@
 import os
 from .settings import *
 
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['*','https://admin-staging.lolab.id',
 #                  'localhost', '10.101.213.97', "https://eirene.netlify.app",
