ea7e81da0d342ff94cbe563429c52cbc68b880e4
dimasinr <dimas.indra773@gmail.com>
2023-02-16 17:48:15 +0700

    DA

--- backendPeti/notes/models.py
+++ backendPeti/notes/models.py
@@ -15,21 +15,21 @@ class NotesHrd(models.Model):
     date_note = models.DateField(null=True, blank=True)
     notes = models.TextField(max_length=230, null=True)
     type_notes = models.CharField(max_length=120, null=True, blank=True)
-    # day = models.IntegerField(null=True, blank=True)
-    # month = models.IntegerField(null=True, blank=True)
-    # years = models.IntegerField(null=True, blank=True)
+    hari = models.IntegerField(null=True, blank=True)
+    bulan = models.IntegerField(null=True, blank=True)
+    tahun = models.IntegerField(null=True, blank=True)
     created_at = models.DateTimeField(default=utils.timezone.now)
     updated_at = models.DateTimeField(auto_now= True)
     # notes_optional = models.TextField(max_length=230, null=True, blank=True, default="optional")
 
-    # def save(self, *args, **kwargs):
-    #         if(self.date_note != None ):
-    #             self.day = self.date_note.day
-    #             self.month = self.date_note.month
-    #             self.years = self.date_note.year
-    #             super(NotesHrd, self).save(*args, **kwargs)
-    #         else:
-    #             super(NotesHrd, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+            if(self.date_note != None ):
+                self.hari = self.date_note.day
+                self.bulan = self.date_note.month
+                self.tahun = self.date_note.year
+                super(NotesHrd, self).save(*args, **kwargs)
+            else:
+                super(NotesHrd, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.employee_name
