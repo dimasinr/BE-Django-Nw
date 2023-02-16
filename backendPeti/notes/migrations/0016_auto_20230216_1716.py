edeb65678f58762f58f4e2358a6cfc54c8f3b811
dimasinr <dimas.indra773@gmail.com>
2023-02-16 17:20:28 +0700

    haha

--- backendPeti/notes/migrations/0016_auto_20230216_1716.py
+++ backendPeti/notes/migrations/0016_auto_20230216_1716.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('notes', '0015_auto_20230216_1711'),
+        ('notes', '0015_auto_20230216_1714'),
     ]
 
     operations = [
