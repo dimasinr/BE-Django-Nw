e396593bc884aa7397399996306063027a2757cf
dimasinr <dimas.indra773@gmail.com>
2023-02-16 16:23:37 +0700

    das

--- backendPeti/notes/models.py
+++ backendPeti/notes/models.py
@@ -15,18 +15,18 @@ class NotesHrd(models.Model):
     date_note = models.DateField(null=True, blank=True)
     notes = models.TextField(max_length=230, null=True)
     type_notes = models.CharField(max_length=120, null=True, blank=True)
-    day_notes = models.IntegerField(null=True, blank=True)
-    month_notes = models.IntegerField(null=True, blank=True)
-    year_notes = models.IntegerField(null=True, blank=True)
+    days = models.IntegerField(null=True, blank=True)
+    months = models.IntegerField(null=True, blank=True)
+    years = models.IntegerField(null=True, blank=True)
     created_at = models.DateTimeField(default=utils.timezone.now)
     updated_at = models.DateTimeField(auto_now= True)
     notes_optional = models.TextField(max_length=230, null=True, blank=True, default="optional")
 
     def save(self, *args, **kwargs):
             if(self.date_note != None ):
-                self.day_notes = self.date_note.day
-                self.month_notes = self.date_note.month
-                self.year_notes = self.date_note.year
+                self.days = self.date_note.day
+                self.months = self.date_note.month
+                self.years = self.date_note.year
                 super(NotesHrd, self).save(*args, **kwargs)
             else:
                 super(NotesHrd, self).save(*args, **kwargs)
