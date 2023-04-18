642631edd8a5b1a1cedf3115fbcbe04076d9211e
dimasinr <dimas.indra773@gmail.com>
2023-04-19 06:07:29 +0700

    reg

--- backendPeti/submisssion/admin.py
+++ backendPeti/submisssion/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Submission
+from .models import Submission, CalendarCutiSubmission
 
 admin.site.register(Submission)
+admin.site.register(CalendarCutiSubmission)
