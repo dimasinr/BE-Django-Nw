c1c3ef16ac48a006d5d7918d5ef9922681ab2d78
dimasinr <dimas.indra773@gmail.com>
2023-04-18 10:04:39 +0700

    update filter by id in notification

--- backendPeti/submisssion/utils/api_notification.py
+++ backendPeti/submisssion/utils/api_notification.py
@@ -27,13 +27,13 @@ def sendNotificationEmployee(permission, jumlahHari, startDate ):
     responses = requests.post(url, json=payload, headers=headers)
     return responses
 
-def sendNotificationHR(permission, jumlahHari, startDate, employee_id, name ):
+def sendNotificationHR(permission, jumlahHari, startDate, employee_id ):
     if(permission != 'lembur'):
             messages = f'Update terbaru Pengajuan untuk perizinan {permission} selama {jumlahHari} hari '
     else:
         messages = f'Update terbaru untuk Pengajuan {permission} pada tanggal {formatDate(startDate)}'
     
-    user_filter = filteruser(pk=employee_id, name=name)
+    user_filter = filteruser(pk=employee_id)
     url = 'https://onesignal.com/api/v1/notifications'
     payload = {
         'app_id': settings.ONESIGNAL_APP_ID,
