d6f0ad2053d2168cf583c78e753e45cc664e7e37
dimas indra <dimas.indra773@gmail.com>
2023-09-09 12:34:42 +0700

    fsafas

--- backendPeti/presenceEmployee/api/serializers.py
+++ backendPeti/presenceEmployee/api/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
-from attendanceEmployee.models import AttendanceEmployee
+from presenceEmployee.models import PresenceEmployee
 from userapp.serializer import UserDetailsSerializer
 
 class PresenceEmployeeSerializers(serializers.ModelSerializer):
     employee = UserDetailsSerializer(read_only=True)
 
     class Meta:
-        model = AttendanceEmployee
+        model = PresenceEmployee
         fields = ['id', 'employee', 'working_date', 'start_from', 'end_from','working_hour',
-                  'lembur_start', 'lembur_end','lembur_hour', 'years', 'months', 'days', 'ket']
+                  'lembur_start', 'lembur_end','lembur_hour', 'years', 'months', 'days', 'ket', 'is_lock']
         index_together = [
             ('employee', 'working_date'),
             ('working_date', 'start_from'),
