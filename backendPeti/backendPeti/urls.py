58dedf3be9116ba7d0b3d1ced614a6ca3c80a24f
dimas indra <dimas.indra773@gmail.com>
2023-10-01 01:00:12 +0700

    fix url

--- backendPeti/backendPeti/urls.py
+++ backendPeti/backendPeti/urls.py
@@ -49,6 +49,59 @@ urlpatterns = (
     # path('rest-auth/', include('rest_auth.urls')),
     # path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('api/reset-password/', UserPasswordReset.as_view(), name='reset-password'),
+    path('api/reset-password/<str:encoded_pk>/<str:token>/',
+         ResetPassword.as_view(), name='reset-password'),
+    path('', include('loginUser.api.urls')),
+
+    path('petitions/', include('pengajuanEmp.api.urls')),
+    path('notes/', include('notes.api.urls')),
+
+    path('users/', include('userapp.api.urls')),
+    path('users/employee/search/', UserSearch.as_view()),
+    path('users/employee/contract/', UserSearchContract.as_view()),
+    path('users/employee/name/', UserSearchView.as_view()),
+    path('users/employee-total/<int:year>', views.UserTotal.as_view()),
+    path('users/employee/cuti/<int:emp_id>/<int:year>/', get_cuti),
+    path('users/employee/change-password/', ChangePasswordAPIView.as_view()),
+    path('employee/chart-pendidikan/', CertificatePieChartAPIView.as_view()),
+
+    path('cuti/', include('saldoCuti.api.urls')),
+
+    path('attendance/', include('attendanceEmployee.api.urls')),
+    path('attendance/employee-sea/', AttendanceAPISearch.as_view()),
+    path('attendance/employee-analysis/', AttendanceAPIAnalisis.as_view()),
+    path('attendance/employee/compare/', AttendanceAPICompare.as_view()),
+    path('attendance/total-day/', TopAttendanceAPIView.as_view()),
+
+    path('api/presence/', include('presenceEmployee.api.urls')),
+    path('api/presence/employee/search', PresenceSearch.as_view()),
+    path('api/presence/employee/compare', PresenceAPICompare.as_view()),
+    path('api/presence/employee/analysis/', PresenceAPIAnalisis.as_view()),
+    path('api/presence/total-day/', TopPresenceAPIView.as_view()),
+    path('api/presence/generate-wfh/', PresenceWFHGenerate.as_view()),
+    path('api/presence/lock/', PresenceLocked.as_view()),
+
+    path('api/note/', include('noteHR.api.urls')),
+    path('api/note/delete/', post_delete_notes),
+
+    path('api/dashboard/', include('calendarDash.api.urls')),
+    path('api/dashboard/week-of', WeekTotals.as_view()),
+    path('api/dashboard/day-of/delete/', post_delete_calendar),
+    path('api/dashboard/employee-permission/', SubmissionIzin.as_view()),
+    path('api/employee/best_of/', UserWorkHourAPIView.as_view()),
+    path('api/dashboard/employee-birth/<int:month>/', EmployeeBirth.as_view()),
+    path('api/dashboard/contract-end/<int:year>/',
+         EmployeeContractEnd.as_view()),
+    path('api/dashboard/statistik-presence/<int:year>/',
+         StatistikPresenceInMonth.as_view()),
+    path('api/dashboard/statistik-submission/<int:year>/',
+         StatistikSubmissionEmployeeInMonth.as_view()),
+    path('api/dashboard/employee-statistik/<int:month>/<int:year>/',
+         PresenceStatistikUser.as_view()),
+
+    path('api/submission/', include('submisssion.api.urls')),
+    path('api/submission/calendar', CalendarSubmissionView.as_view()),
+
     path('api/send-notification/', send_notification_api, name='send_notification'),
 
 ]
